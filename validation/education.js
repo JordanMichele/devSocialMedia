f87d1c4ba05b73b97e361d563dba43c172a981a8
JordanMichele <jordanmichele32@gmail.com>
2018-10-03 08:58:33 -0400

    added education route

--- validation/education.js
+++ validation/education.js
@@ -8,17 +8,22 @@ module.exports = function validateEducationInput(data) {
   data.school = !isEmpty(data.school) ? data.school : "";
   data.degree = !isEmpty(data.degree) ? data.degree : "";
   data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
 
-  if (Validator.isEmpty(data.title)) {
-    errors.title = "Job Title is required";
+  if (Validator.isEmpty(data.school)) {
+    errors.school = "School field is required";
   }
 
-  if (Validator.isEmpty(data.company)) {
-    errors.company = "Company is required";
+  if (Validator.isEmpty(data.degree)) {
+    errors.company = "Degree field is required";
   }
 
   if (Validator.isEmpty(data.from)) {
     errors.from = "From date field is required";
+  }
+
+  if (Validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = "Field of study field is required";
   }
 
   // Return errors or valid
